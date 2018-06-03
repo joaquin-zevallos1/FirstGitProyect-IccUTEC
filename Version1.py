443e2fed0136096a7b77b4e7d66c668822794bf9
Kelvin <kelvin.morales@utec.edu.pe>
2018-06-03 01:44:50 -0500

    Código: sumar edades mayor y menor de una lista.

--- Version1.py
+++ Version1.py
@@ -1,1 +1,13 @@
-n = input("Aprendiendo GitHub en ICC-UTEC")
+e=int(input())
+listan=[]
+i=0
+while i<e:
+    listan.append(input("Ingresa su edad: "))
+    i=i+1
+mayor=int(max(listan))
+menor=int(min(listan))
+suma = str(mayor)+ str(menor)
+print(listan)
+print(mayor)
+print(menor)
+print(mayor+menor)
